5311382016c345ef09bd4249e446f469abfca09f
mintarchit <mintarchit@gmail.com>
2023-02-18 03:45:36 +0100

    init

---
+++ lsrepo.sh
@@ -0,0 +1,13 @@
+gh api graphql --paginate -f query='
+  query($endCursor: String) {
+    viewer {
+      repositories(first: 100, after: $endCursor) {
+        nodes { nameWithOwner }
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+      }
+    }
+  }
+' --jq '..|.nameWithOwner?' | grep . | bat
